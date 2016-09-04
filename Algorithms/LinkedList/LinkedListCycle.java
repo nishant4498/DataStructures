0e9c7428c6511c43f17f0538c31667cbb66d02cb
Nishant Kumar <nishant4498@gmail.com>
2016-09-04 09:33:02 -0700

    Added new problems

---
+++ Algorithms/LinkedList/LinkedListCycle.java
@@ -0,0 +1,17 @@
+
+public class LinkedListCycle {
+	public static boolean hasCycle(ListNode head) {
+		ListNode slowPtr = head;
+		ListNode fastPtr = head;
+
+		while (fastPtr != null && fastPtr.next != null) {
+			slowPtr = slowPtr.next;
+			fastPtr = fastPtr.next.next;
+
+			if (slowPtr == fastPtr)
+				return true;
+		}
+
+		return false;
+	}
+}
